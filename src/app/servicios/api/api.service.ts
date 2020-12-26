77867b1ec53a6efb57d124a3a0e2bde171745c82
kathy <katherine.rovayo9809@utc.edu.ec>
2020-12-26 17:24:51 -0500

    lista de productos

--- src/app/servicios/api/api.service.ts
+++ src/app/servicios/api/api.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
-
+import { Observable } from 'rxjs';
+import {ListaproductosI} from '../../modelos/listaProductos.interface'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+  private url:string="http://127.0.0.1:8000/productos/";
+  constructor(private http:HttpClient) { }
+  getAllproductos():Observable<ListaproductosI[]>{
+    return this.http.get<ListaproductosI[]>(this.url);
+  }
 }
