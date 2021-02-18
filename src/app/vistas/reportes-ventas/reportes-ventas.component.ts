bf2e5c4e1f6bf79d462c046bd3c69c2b0a03b017
kathy <katherine.rovayo9809@utc.edu.ec>
2021-02-18 14:05:03 -0500

    reporte de ventas

--- src/app/vistas/reportes-ventas/reportes-ventas.component.ts
+++ src/app/vistas/reportes-ventas/reportes-ventas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ventaI } from 'src/app/modelos/regventa.interface';
 // importo mi servicios sandoval
 import {ApiService} from '../../servicios/api/api.service';
 //fin
@@ -9,10 +10,28 @@ import {ApiService} from '../../servicios/api/api.service';
   styleUrls: ['./reportes-ventas.component.css']
 })
 export class ReportesVentasComponent implements OnInit {
-
+  ventas:ventaI[];
   constructor(public api:ApiService) { }
   ngOnInit(): void {
-    this.api.getAllVentas()
+    
+    
   }
+  selectFecha(){
+    this.api.getAllventa().subscribe(data => {
+      var ventaL=[];
+      var FechaIni=(<HTMLInputElement>document.getElementById('inicioFechaReporte')).value;
+      var FechaFin=(<HTMLInputElement>document.getElementById('finFechaReporte')).value;
+        for(let posicion in data){
+          console.log(data[posicion].fecha.substr(0, 10));
+          if (data[posicion].fecha >= FechaIni && data[posicion].fecha.substr(0, 10) <= FechaFin){
+            ventaL.push(data[posicion]);
+            console.log(data[posicion].fecha);
+            this.ventas=ventaL;
+          }
+        }
+        console.log(ventaL);
+    })
+
+ }
 
 }
