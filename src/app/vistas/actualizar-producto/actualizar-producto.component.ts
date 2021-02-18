98367ffeaf51684f8edc3b01658ac949f77dda27
aleandro <bryan-recors@hotmail.com>
2021-02-17 19:27:36 -0500

    modulo productos terminado ya registra y actualiza correctamente

--- src/app/vistas/actualizar-producto/actualizar-producto.component.ts
+++ src/app/vistas/actualizar-producto/actualizar-producto.component.ts
@@ -4,7 +4,7 @@ import {productoI} from '../../modelos/producto.interface';
 import {ApiService} from '../../servicios/api/api.service';
 import {FormGroup, FormControl,Validators} from '@angular/forms';
 import {ListaproveedoresI} from '../../modelos/listaProveedores.interface';
-import {proveedorI} from '../../modelos/proveedor.interface';
+
 @Component({
   selector: 'app-actualizar-producto',
   templateUrl: './actualizar-producto.component.html',
@@ -13,10 +13,10 @@ import {proveedorI} from '../../modelos/proveedor.interface';
 
 export class ActualizarProductoComponent implements OnInit {
   constructor(private activerouter:ActivatedRoute, private router:Router, private api:ApiService ) { }
-
+  //proveedor
   proveedores:ListaproveedoresI[];
+ //producto
   datosProducto:productoI;
-  datosProveedor:proveedorI;
   editarForm = new FormGroup({
     id:new FormControl(''),
     nombre:new FormControl(''),
@@ -24,31 +24,35 @@ export class ActualizarProductoComponent implements OnInit {
     precio:new FormControl(''),
     stock:new FormControl(''),
     proveedor:new FormControl(''),
-
   });
 
   ngOnInit(): void {
     //obtener el id
     this.consultarproveedores();
     let productoid=this.activerouter.snapshot.paramMap.get('id');
-    console.log("este es id de producto");
-    console.log(productoid);
+    //console.log("este es id de producto");
+    //console.log(productoid);
     this.api.getSingleProductos(productoid).subscribe(data =>{
-      console.log(data);
+      //console.log(data);
       this.datosProducto=data;
-      this.datosProveedor=data.proveedor;
-      //console.log("mm");
-      //console.log(this.datosProducto);
+      //var indice:number = 0;
+      var nombreprov:string = '';
+      for(let posicion in this.proveedores){
+        if (this.datosProducto.proveedor == this.proveedores[posicion].id){
+        //  indice = Number(this.proveedores[posicion].id)
+          nombreprov = this.proveedores[posicion].nombre
+        }
+      }
       this.editarForm.setValue({
         'id':productoid,
         'nombre':this.datosProducto.nombre,
         'descripcion':this.datosProducto.descripcion,
         'precio':this.datosProducto.precio,
         'stock':this.datosProducto.stock,
-        'proveedor':this.datosProveedor.nombre,
+        'proveedor':nombreprov,
       });
-      console.log("editar form");
-      console.log(this.editarForm.value);
+      //console.log("editar form");
+      //console.log(this.datosProducto.proveedor);
     })
   }
 
@@ -56,16 +60,28 @@ export class ActualizarProductoComponent implements OnInit {
   consultarproveedores(){
     this.api.getAllProveedores().subscribe(data => {
       this.proveedores=data;
-      console.log(this.proveedores);
+      //console.log(this.proveedores);
     })
   }
 
   postForm(form:productoI){
-    console.log(form)
+    //console.log("editar formque tiene");
+    //console.log(form)
+    var indice:number = 0;
+    for(let posicion in this.proveedores){
+      if (form.proveedor == this.proveedores[posicion].nombre){
+        indice = Number(this.proveedores[posicion].id)
+      }
+    }
+    //console.log("lo encontre");
+    //console.log(indice);
+    form.proveedor=indice;
+    //console.log(form)
     this.api.putProducto(form).subscribe(data =>{
       console.log(data);
     });
     this.router.navigate(['listar-productos']);
   }
+
 
 }
