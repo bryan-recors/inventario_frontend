d9697deda8e5c23340db7bd588a2c217c6081d02
Johana-Sanchez <76419490+Johana-Sanchez@users.noreply.github.com>
2021-02-18 20:15:57 -0500

    Johana (#15)
    * buscar en productos
    
    * buscar productos-proveedores-usuarios y cambios en index
    
    * buscar en reportes
    
    * filtro en reporte de productos

---
+++ src/app/pipes/filter-reportes-productos.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FilterReportesProductosPipe } from './filter-reportes-productos.pipe';
+
+describe('FilterReportesProductosPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FilterReportesProductosPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
